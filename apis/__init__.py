2d33f494a39148471aea3b531db8b6952f5f096a
Avkash Kakdiya <avkash@Avkashs-MacBook-Pro.local>
2019-09-04 21:36:36 +0530

    removed extra modules

--- apis/__init__.py
+++ apis/__init__.py
@@ -7,9 +7,7 @@ https://flask-restplus.readthedocs.io/en/stable/api.html
 from flask_restplus import Api
 from flask import Blueprint
 
-from .user.route import api as ns1
-from .todo.route import api as ns2
-from .post.routes import ns as ns3
+from .post.routes import ns as NSpost
 
 
 bp_api_v1 = Blueprint('api', __name__)
@@ -26,9 +24,7 @@ api = Api(
     # All API metadatas
 )
 
-api.add_namespace(ns1)
-api.add_namespace(ns2)
-api.add_namespace(ns3)
+api.add_namespace(NSpost)
 
 #api.add_namespace(ns1, path='/api/v1/users')
 #api.add_namespace(ns2, path='/api/v1/todos')
